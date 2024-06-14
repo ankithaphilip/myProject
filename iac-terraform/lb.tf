2125dd5d155fc02b670082e5372aff9d81485136
ankithaphilip <43670027+ankithaphilip@users.noreply.github.com>
2024-06-14 12:24:09 +0530

    Update lb.tf

--- iac-terraform/lb.tf
+++ iac-terraform/lb.tf
@@ -9,7 +9,7 @@ resource "aws_lb_target_group" "target_group" {
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.default.id
-  target_type = "ip"
+  target_type = "ip" 
 }
 
 resource "aws_lb_listener" "listener" {
